b3768ebd1dd77c7bc1e4aa12d7104c7daf5e9cfa
VishvaLuvani <vishva.luvani@pixer.digital>
2026-05-01 12:24:13 +0530

    Fix feedbacks for Exercise 1

---
+++ Newcss/js/common.js
@@ -0,0 +1,10 @@
+// Navbar toggle functionality
+function toggleMenu() {
+  const modal = document.getElementById("menuModal");
+
+  if (modal.style.display === "block") {
+    modal.style.display = "none";
+  } else {
+    modal.style.display = "block";
+  }
+}
